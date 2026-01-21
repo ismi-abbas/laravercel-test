b321454acb5af4f00362f084611439afcc399a16
Abbas <abbaspuzi.dev@gmail.com>
2026-01-21 23:38:47 +0800

    init

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<headt>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta title="Orders Management">
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @inertiaHead
+</headt>
+<body>
+@inertia
+</body>
+</html>
